d46e1c305fc18d8d03c0a70901f2758a7ac57670
naytsyrhc <naytsyrhc@gmx.org>
2024-01-09 13:31:29 +0100

    Status in navigation now and self-contained.

--- boot/src/main/resources/javascript/pages/Dbbc.jsx
+++ boot/src/main/resources/javascript/pages/Dbbc.jsx
@@ -7,39 +7,22 @@ import {
     AlertIcon,
     AlertTitle,
     Box,
-    Flex,
     FormControl,
     FormLabel,
     Select,
-    Spacer,
     Spinner,
     Stack,
     Text
 } from "@chakra-ui/react";
 import config from "../config";
 import Navigation from "../components/Navigation";
-import Status from "../components/Status";
 
 function Dbbc() {
     const [teams, setTeams] = useState([]);
     const [leagues, setLeagues] = useState([]);
     const [leagueId, setLeagueId] = useState("");
     const [loading, setLoading] = useState(false);
-    const [status, setStatus] = useState();
     const [error, setError] = useState("");
-
-    useEffect(() => {
-        const fetchStatus = () => {
-            CyanideApiService.status().then((data) => {
-                setStatus(data)
-            })
-                .catch((reason) => {
-                    setError(reason.toLocaleString(config.locale));
-                })
-        }
-        setInterval(() => fetchStatus(), 30_000);
-        fetchStatus();
-    }, []);
 
     useEffect(() => {
         const fetchLeagues = () => {
@@ -82,11 +65,7 @@ function Dbbc() {
 
     return <Stack>
         <Box>
-            <Flex>
-                <Navigation currentPage="home" status={status}/>
-                <Spacer/>
-                <Status status={status}/>
-            </Flex>
+            <Navigation currentPage="home"/>
         </Box>
         <Box>
             <FormControl>
