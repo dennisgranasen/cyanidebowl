30b12632fd8f5810bbac587beeee4f3b74d54fe4
naytsyrhc <naytsyrhc@gmx.org>
2024-01-22 11:36:24 +0100

    Some work to get prod ready.

--- boot/src/main/java/net/warp_scores/warpscores/domain/persistence/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package net.warp_scores.warpscores.domain.persistence;
-
-import net.warp_scores.warpscores.domain.model.Status;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusRepository extends MongoRepository<Status, String> {
-}
