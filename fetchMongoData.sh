aa95c402907343b21e3a3ad9e30be01db60683dc
naytsyrhc <naytsyrhc@gmx.org>
2024-06-09 22:59:16 +0200

    Added repair after downloading db.

--- fetchMongoData.sh
+++ fetchMongoData.sh
@@ -4,8 +4,10 @@ docker-compose down
 
 cd data || exit 1
 
-sudo -E rsync -rtvzv vserver.wawuschels.de:/var/customers/webs/christian/warpscores_net/data/* ./
+sudo -E rsync -rtvzv --exclude mongod.lock --exclude WiredTiger.lock vserver.wawuschels.de:/var/customers/webs/christian/warpscores_net/data/* ./
 
 cd ..
+
+docker run -v ./data:/data/db mongo:7.0.5 mongod --repair
 
 docker-compose up -d
