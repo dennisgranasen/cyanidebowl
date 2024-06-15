efdc1b95d3a3965020758c64a1872cef838067b8
naytsyrhc <naytsyrhc@gmx.org>
2024-06-15 23:25:47 +0200

    current work

--- frontend/src/components/misc/Version.jsx
+++ frontend/src/components/misc/Version.jsx
@@ -1,13 +1,11 @@
-import { Box, HStack, VStack } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import CyanideApiService from '../../CyanideApiService';
 import config from '../../config';
-import version from '../../version.json';
 import abbreviators from '../../util/Abbreviators';
 
 function Version({ headerSize, textSize, ...props }) {
   const [backendVersion, setBackendVersion] = useState(null);
-  const [frontendVersion, setFrontendVersion] = useState(null);
   const fetchBackendVersion = () => {
     CyanideApiService.backendVersion()
       .then((data) => {
@@ -17,30 +15,17 @@ function Version({ headerSize, textSize, ...props }) {
         setBackendVersion(reason.toLocaleString(config.locale));
       });
   };
-  const fetchFrontendVersion = () => {
-    setFrontendVersion(version);
-  };
 
   useEffect(() => {
     fetchBackendVersion();
-    fetchFrontendVersion();
   }, []);
 
   return (
     <Box fontSize={headerSize} {...props}>
-      <HStack align="left">
-        <Box fontSize={textSize}>
-          {backendVersion &&
-            `B: ${backendVersion.projectVersion} (${abbreviators.abbreviateText(backendVersion.committish, 6)})`}
-        </Box>
-        <Box fontSize={textSize}>
-          {frontendVersion &&
-            `F: ${frontendVersion.projectVersion} (${abbreviators.abbreviateText(
-              frontendVersion.committish,
-              6
-            )})`}
-        </Box>
-      </HStack>
+      <Box fontSize={textSize}>
+        {backendVersion && backendVersion.projectVersion &&
+          `B: ${backendVersion.projectVersion} (${abbreviators.abbreviateText(backendVersion.committish, 6)})`}
+      </Box>
     </Box>
   );
 }
