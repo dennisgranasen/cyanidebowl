2aaf18845cad764815b52a1a3cf04018378d0f68
naytsyrhc <naytsyrhc@gmx.org>
2024-06-19 11:05:02 +0200

    Do not render accordian if not wanted.

--- frontend/src/components/common/HeaderCard.jsx
+++ frontend/src/components/common/HeaderCard.jsx
@@ -36,6 +36,7 @@ function StandardScreenHeaderCard(mainImageSrc, heading, subHeading, additionalI
 }
 
 function SmallScreenHeaderCard(mainImageSrc, heading, subHeading, detailsHeading, additionalImageSrc, children) {
+  const noAccordion = !detailsHeading;
   return (
     <Card direction="column">
       <Box p="10px">
@@ -46,17 +47,20 @@ function SmallScreenHeaderCard(mainImageSrc, heading, subHeading, detailsHeading
           <Box flex="1" overflow="hidden">
             <Heading>{heading}</Heading>
             <Box mb="10px">{subHeading}</Box>
-            <Accordion allowMultiple>
-              <AccordionItem>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    {detailsHeading}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel>{children}</AccordionPanel>
-              </AccordionItem>
-            </Accordion>
+            {noAccordion && children}
+            {!noAccordion && (
+              <Accordion allowMultiple defaultIndex={!detailsHeading ? [0] : null}>
+                <AccordionItem>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      {detailsHeading}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                  <AccordionPanel>{children}</AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            )}
           </Box>
         </Flex>
       </CardBody>
