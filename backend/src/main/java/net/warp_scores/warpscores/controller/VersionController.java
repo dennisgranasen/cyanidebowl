d01cd7aba379e0dd019b2a39a10c92769402da79
naytsyrhc <naytsyrhc@gmx.org>
2024-07-14 22:37:07 +0200

    Fixed generation of version.json

---
+++ backend/src/main/java/net/warp_scores/warpscores/controller/VersionController.java
@@ -0,0 +1,48 @@
+package net.warp_scores.warpscores.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.warp_scores.warpscores.cyanide.api.model.common.CompetitionStatus;
+import net.warp_scores.warpscores.domain.persistence.LeagueRepository;
+import net.warp_scores.warpscores.model.League;
+import net.warp_scores.warpscores.service.CompetitionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class VersionController {
+
+    private final LeagueRepository leagueRepository;
+
+    private final CompetitionService competitionService;
+
+    @GetMapping("/league")
+    public ResponseEntity<List<League>> getLeagues() {
+        List<League> all = leagueRepository.findAll();
+        all.forEach(this::countCompetitions);
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/league/{leagueUuid}")
+    public ResponseEntity<League> getLeague(@PathVariable(name = "leagueUuid") UUID leagueUuid) {
+        Optional<League> league = leagueRepository.findById(leagueUuid);
+        league.ifPresent(this::countCompetitions);
+        return league
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    private void countCompetitions(League league) {
+        Map<CompetitionStatus, Long> countsByStatus = competitionService.countForLeague(league.getUuid());
+        league.setCountsByCompetitionStatus(countsByStatus);
+    }
+}
