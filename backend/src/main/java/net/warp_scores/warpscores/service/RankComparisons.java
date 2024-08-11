ed5773c8071d094457d9247bbafce37c3e131c50
naytsyrhc <naytsyrhc@gmx.org>
2024-08-11 12:44:42 +0200

    Slightly changed order of columns in rank, new RankService and RankComparisons. Adjusted default comparisons to match BB3 ingame rank.

---
+++ backend/src/main/java/net/warp_scores/warpscores/service/RankComparisons.java
@@ -0,0 +1,71 @@
+package net.warp_scores.warpscores.service;
+
+import lombok.Getter;
+import net.warp_scores.warpscores.model.Rank;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.Random;
+
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingInt;
+import static java.util.Optional.ofNullable;
+
+@Getter
+public enum RankComparisons {
+    SCORE_310("3:1:0", "By score, won games giving 3, draws 1, losses 0 score.",
+            (rankA, rankB) -> ofNullable(rankB.getGamesWon()).orElse(0) * 3
+                    + ofNullable(rankB.getGamesDrawn()).orElse(0) * 1
+                    - ofNullable(rankA.getGamesWon()).orElse(0) * 3
+                    - ofNullable(rankA.getGamesDrawn()).orElse(0)
+    ),
+    SCORE_210("2:1:0", "By score, won games giving 2, draws 1, losses 0 score.",
+            (rankA, rankB) -> ofNullable(rankB.getGamesWon()).orElse(0) * 2
+                    + ofNullable(rankB.getGamesDrawn()).orElse(0) * 1
+                    - ofNullable(rankA.getGamesWon()).orElse(0) * 2
+                    - ofNullable(rankA.getGamesDrawn()).orElse(0)
+    ),
+    WINS("Wins", "Just counting won games.",
+            (rankA, rankB) -> ofNullable(rankB.getGamesWon()).orElse(0)
+                    - ofNullable(rankA.getGamesWon()).orElse(0)
+    ),
+    INFLICTED_TOUCHDOWNS("TD+", "Just counting inflicted touchdowns.",
+            (rankA, rankB) -> ofNullable(rankB.getInflictedTouchdowns()).orElse(0)
+                    - ofNullable(rankA.getInflictedTouchdowns()).orElse(0)),
+    INFLICTED_CAS("CAS+", "Just counting inflicted casualties.",
+            (rankA, rankB) -> ofNullable(rankB.getInflictedCasualties()).orElse(0)
+                    - ofNullable(rankA.getInflictedCasualties()).orElse(0)),
+    SUSTAINED_TOUCHDOWNS("TD-", "Just counting sustained touchdowns.",
+            comparingInt(rank -> ofNullable(rank.getSustainedTouchdowns()).orElse(0))),
+    SUSTAINED_CAS("CAS-", "Just counting sustained casualties.",
+            comparingInt(rank -> ofNullable(rank.getSustainedCasualties()).orElse(0))),
+    TOUCHDOWN_DIFFERENCE("TDD", "Touchdown difference.",
+            (rankA, rankB) ->
+                    (ofNullable(rankB.getInflictedTouchdowns()).orElse(0) - ofNullable(
+                            rankB.getSustainedTouchdowns()).orElse(0))
+                            - (ofNullable(rankA.getInflictedTouchdowns()).orElse(0) - ofNullable(
+                            rankA.getSustainedTouchdowns()).orElse(0))
+    ),
+    CAS_DIFFERENCE("CASD", "Casualties difference.",
+            (rankA, rankB) ->
+                    (ofNullable(rankB.getInflictedCasualties()).orElse(0) - ofNullable(
+                            rankB.getSustainedCasualties()).orElse(0))
+                            - (ofNullable(rankA.getInflictedCasualties()).orElse(0) - ofNullable(
+                            rankA.getSustainedCasualties()).orElse(0))),
+    GAMES_PLAYED("GP", "Number of games played.",
+            comparing(rank -> ofNullable(rank.getGamesPlayed()).orElse(0))),
+    CTV("CTV", "Current team value.",
+            comparing(rank -> ofNullable(rank.getTeam().getValue()).orElse(BigDecimal.ZERO))),
+    HEAD_TO_HEAD("H2H", "Head to head between the two teams.", (rankA, rankB) -> 0),
+    RANDOM("RND", "A random tie breaker.", comparingInt(rank -> new Random(rank.hashCode()).nextInt()));
+
+    private final String abbreviation;
+    private final String description;
+    private final Comparator<Rank> comparator;
+
+    RankComparisons(String abbreviation, String description, Comparator<Rank> comparator) {
+        this.abbreviation = abbreviation;
+        this.description = description;
+        this.comparator = comparator;
+    }
+}
---
+++ backend/src/main/java/net/warp_scores/warpscores/service/RankService.java
@@ -0,0 +1,167 @@
+package net.warp_scores.warpscores.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.warp_scores.warpscores.domain.TeamDomainService;
+import net.warp_scores.warpscores.domain.persistence.ContestRepository;
+import net.warp_scores.warpscores.domain.persistence.MatchRepository;
+import net.warp_scores.warpscores.model.Competition;
+import net.warp_scores.warpscores.model.Contest;
+import net.warp_scores.warpscores.model.Match;
+import net.warp_scores.warpscores.model.Rank;
+import net.warp_scores.warpscores.model.Team;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.Objects.requireNonNullElse;
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
+import static net.warp_scores.warpscores.cyanide.api.model.common.MatchStatus.Validated;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RankService {
+    private final MatchRepository matchRepository;
+    private final ContestRepository contestRepository;
+    private final TeamDomainService teamDomainService;
+    private final CompetitionService competitionService;
+
+    private final List<RankComparisons> defaultRankComparisons = asList(RankComparisons.SCORE_310, RankComparisons.WINS,
+            RankComparisons.INFLICTED_TOUCHDOWNS, RankComparisons.TOUCHDOWN_DIFFERENCE);
+
+    public List<Rank> getRanksForCompetition(UUID competitionId, Optional<List<RankComparisons>> rankComparisons) {
+        Competition competition = competitionService.loadCompetition(competitionId)
+                .orElseThrow(NoSuchElementException::new);
+
+        List<Team> teams = teamDomainService.findByCompetitionId(competitionId);
+
+        return teams.stream()
+                .map(team -> toRank(team, competition, rankComparisons.orElse(defaultRankComparisons)))
+                .sorted(
+                        (rankA, rankB) -> {
+                            int result = 0;
+                            for (RankComparisons comparisons : rankComparisons.orElse(defaultRankComparisons)) {
+                                result = comparisons.getComparator().compare(rankA, rankB);
+                                if (result != 0) {
+                                    return result;
+                                }
+                            }
+                            return result;
+                        }
+                )
+                .collect(HashMap<Rank, Integer>::new, (map, rank) -> map.put(rank, map.size() + 1), (map, map2) -> {})
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    entry.getKey().setRank(entry.getValue());
+                    return entry.getKey();
+                })
+                .collect(Collectors.toList());
+    }
+
+    private Rank toRank(Team team, Competition competition, List<RankComparisons> rankComparisons) {
+        Rank rank = new Rank();
+        rank.setTeam(team);
+        List<Contest> contests = contestRepository.findByCompetitionIdAndStatus(competition.getUuid(), Validated);
+        int gamesPlayed = 0;
+        int gamesWon = 0;
+        int gamesDrawn = 0;
+        int gamesLost = 0;
+        int ownMatchScore = 0;
+        int otherMatchScore = 0;
+        int inflictedCasualties = 0;
+        int sustainedCasualties = 0;
+        for (Contest contest : contests) {
+            Optional<Match> match = ofNullable(contest.getMatchUuid()).flatMap(matchRepository::findById);
+
+            List<Team> teamResults = match.map(Match::getTeams).orElse(contest.getOpponents());
+            Optional<Team> ownTeam = getTeam(teamResults, team.getId());
+            Optional<Team> otherTeam = getOtherTeam(teamResults, ownTeam);
+            if (ownTeam.isPresent() && otherTeam.isPresent()) {
+                gamesPlayed++;
+                Team own = ownTeam.get();
+                Team other = otherTeam.get();
+                int currOwnMatchScore = requireNonNullElse(own.getScore(), 0);
+                int currOtherMatchScore = requireNonNullElse(other.getScore(), 0);
+                boolean won = currOwnMatchScore > currOtherMatchScore;
+                boolean lost = currOwnMatchScore < currOtherMatchScore;
+                boolean drawn = currOwnMatchScore == currOtherMatchScore;
+                if (won) {
+                    gamesWon++;
+                }
+                if (lost) {
+                    gamesLost++;
+                }
+                if (drawn) {
+                    gamesDrawn++;
+                }
+                ownMatchScore += currOwnMatchScore;
+                otherMatchScore += currOtherMatchScore;
+                inflictedCasualties += requireNonNullElse(own.getInflictedcasualties(), 0);
+                sustainedCasualties += requireNonNullElse(other.getInflictedcasualties(), 0);
+            }
+        }
+        rank.setScore(calcScore(gamesWon, gamesDrawn, gamesLost, rankComparisons));
+        rank.setGamesPlayed(gamesPlayed);
+        rank.setGamesWon(gamesWon);
+        rank.setGamesDrawn(gamesDrawn);
+        rank.setGamesLost(gamesLost);
+        rank.setInflictedTouchdowns(ownMatchScore);
+        rank.setSustainedTouchdowns(otherMatchScore);
+        rank.setInflictedCasualties(inflictedCasualties);
+        rank.setSustainedCasualties(sustainedCasualties);
+        return rank;
+    }
+
+    private Integer calcScore(int gamesWon,
+            int gamesDrawn,
+            int gamesLost,
+            List<RankComparisons> rankComparisons) {
+        if (rankComparisons.contains(RankComparisons.SCORE_310)) {
+            return (3 * gamesWon) + (1 * gamesDrawn) + (0 * gamesLost);
+        } else if (rankComparisons.contains(RankComparisons.SCORE_210)) {
+            return (2 * gamesWon) + (1 * gamesDrawn) + (0 * gamesLost);
+        }
+        return 0;
+    }
+
+    private Optional<Team> getOtherTeam(List<Team> teamResults, Optional<Team> myTeam) {
+        if (myTeam.isEmpty()) {
+            return empty();
+        }
+        List<Team> teams = teamResults.stream()
+                .filter(team -> !myTeam.get().getId().equals(team.getId()))
+                .toList();
+        if (teams.isEmpty()) {
+            return empty();
+        }
+        if (teams.size() != 1) {
+            throw new IllegalArgumentException("Ambiguous results for other team.");
+        }
+        return Optional.of(teams.get(0));
+    }
+
+    private Optional<Team> getTeam(List<Team> teamResults, UUID teamId) {
+        if (teamResults == null) {
+            return empty();
+        }
+        List<Team> teams = teamResults.stream()
+                .filter(team -> teamId.equals(team.getId()))
+                .toList();
+        if (teams.isEmpty()) {
+            return empty();
+        }
+        if (teams.size() != 1) {
+            throw new IllegalArgumentException("Ambiguous results for other team.");
+        }
+        return Optional.of(teams.get(0));
+    }
+}
