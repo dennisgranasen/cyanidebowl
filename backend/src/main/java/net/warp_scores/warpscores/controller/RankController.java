1d4fdf8968c80df57f22973303cd4f5a39329d2a
naytsyrhc <naytsyrhc@gmx.org>
2024-08-18 00:54:06 +0200

    Fixed 404 notFound for no rankings.

--- backend/src/main/java/net/warp_scores/warpscores/controller/RankController.java
+++ backend/src/main/java/net/warp_scores/warpscores/controller/RankController.java
@@ -1,6 +1,7 @@
 package net.warp_scores.warpscores.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.warp_scores.warpscores.model.Rank;
 import net.warp_scores.warpscores.service.RankService;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class RankController {
 
     private final RankService rankService;
@@ -22,17 +25,20 @@ public class RankController {
     @GetMapping("/ranks/competition/{competitionId}")
     public ResponseEntity<List<Rank>> getRanksForCompetition(@PathVariable(name = "competitionId") UUID competitionId) {
         if (competitionId == null) {
+            log.error("competitionId is null");
             return ResponseEntity.badRequest().build();
         }
         try {
             List<Rank> ranks = rankService.getRanksForCompetition(competitionId, Optional.empty());
             if (ranks == null || ranks.isEmpty()) {
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.ok(Collections.emptyList());
             }
             return ResponseEntity.ok(ranks);
         } catch (NoSuchElementException ex) {
+            log.error("Caught NoSuchElementException.", ex);
             return ResponseEntity.badRequest().build();
         } catch (Exception ex) {
+            log.error("Caught Exception.", ex);
             return ResponseEntity.internalServerError().build();
         }
     }
