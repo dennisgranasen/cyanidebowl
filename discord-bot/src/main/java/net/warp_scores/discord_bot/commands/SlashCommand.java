d8b13e346072dd9e066cb3721b6840dfc5c3928e
naytsyrhc <naytsyrhc@gmx.org>
2024-08-21 10:35:01 +0200

    Fixed help message. Fixed registration of commands.

--- discord-bot/src/main/java/net/warp_scores/discord_bot/commands/SlashCommand.java
+++ discord-bot/src/main/java/net/warp_scores/discord_bot/commands/SlashCommand.java
@@ -1,6 +1,7 @@
 package net.warp_scores.discord_bot.commands;
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.spec.EmbedCreateSpec;
 import reactor.core.publisher.Mono;
 
 public interface SlashCommand {
