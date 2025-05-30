1f5e9114637eb3aacf3941a07b910e69e03fb4d1
Dennis <dennis.granasen@gmail.com>
2025-05-30 23:30:11 +0200

    preparing to fly

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,6 +1,6 @@
 const MAX_AGE_FOR_STATUS_IN_MILLIS = 35 * 60 * 1_000;
 const isProduction = process.env.NODE_ENV === 'production';
-const backendUrl = isProduction ? 'https://warp-scores.net/api' : 'http://localhost:18080';
+const backendUrl = isProduction ?  process.env.BACKEND_URI : 'http://localhost:8080';
 const auth0ClientId = 'vds4UOVhMmdut8kNXOQ0XJ1s5st95vdu';
 const auth0Domain = 'warp-scores.eu.auth0.com';
 const auth0Audience = 'warp-scores-backend';
