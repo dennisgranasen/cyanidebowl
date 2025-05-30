229c58cbd807f54fb83d8ac1824220218c3f7a65
Dennis <dennis.granasen@gmail.com>
2025-05-31 00:31:09 +0200

    temp fix: hardcoded backend url instead

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,16 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
-const webpack = require('webpack');
-
-// In your plugins array:
-plugins: [
-  // ...other plugins,
-  new webpack.DefinePlugin({
-    'process.env.REACT_APP_BACKEND_URI': JSON.stringify(process.env.REACT_APP_BACKEND_URI),
-  }),
-],
-
 module.exports = merge(common, {
   mode: 'production',
 });
