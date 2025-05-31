5d7152c8d803f5c8b993b3acba968c0bd92f6400
Dennis <dennis.granasen@gmail.com>
2025-05-31 19:55:32 +0200

    Removed the require https thingy, it's probably not needed

--- backend/src/main/java/net/warp_scores/warpscores/config/SecurityConfiguration.java
+++ backend/src/main/java/net/warp_scores/warpscores/config/SecurityConfiguration.java
@@ -41,9 +41,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .requiresChannel(channel -> channel
-                   .anyRequest().requiresSecure()
-                )       
                 .authorizeHttpRequests(requests -> requests
                         // status/misc
                         .requestMatchers(GET, "/version.json", "/status").permitAll()
