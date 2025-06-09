fa46cf1f93c6c6790d80d005c34bcfebfb30d051
Dennis <dennis.granasen@gmail.com>
2025-06-09 08:39:27 +0200

    bb2 comp loading updates

--- api/src/main/java/net/warp_scores/warpscores/model/CompetitionFormat.java
+++ api/src/main/java/net/warp_scores/warpscores/model/CompetitionFormat.java
@@ -1,5 +1,6 @@
 package net.warp_scores.warpscores.model;
 
 public enum CompetitionFormat implements Comparable<CompetitionFormat> {
+    /** TODO: Need to update for BB2 */
     RoundRobin, Knockout, Wissen, Ladder, Arena
 }
