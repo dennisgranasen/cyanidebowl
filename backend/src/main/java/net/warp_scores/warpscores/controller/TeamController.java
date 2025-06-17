6f63d6fb77a253388524a21ac4e43e7a83e0bded
Dennis Granåsen <dennis.granasen@gmail.com>
2025-06-17 17:05:24 +0200

    still broken

--- backend/src/main/java/net/warp_scores/warpscores/controller/TeamController.java
+++ backend/src/main/java/net/warp_scores/warpscores/controller/TeamController.java
@@ -10,6 +10,7 @@ import net.warp_scores.warpscores.service.OfficialLeagueAndCompetitions;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,22 +29,28 @@ public class TeamController {
     private final OfficialLeagueAndCompetitions officialLeagueAndCompetitions;
 
     @GetMapping("/teams/{teamId}")
-    public ResponseEntity<Team> getTeam(@PathVariable(name = "teamId") String teamId) {
+    public ResponseEntity<Team> getTeam(
+            @PathVariable(name = "teamId") String teamId,
+            @RequestParam(name = "opus", required = false) Integer opus ) {
         try {
-            Optional<Team> team = teamDomainService.findTeam(teamId, Optional.empty());
+            Optional<Team> team = teamDomainService.findTeam(teamId, Optional.empty(),
+                Optional.ofNullable(opus));
             return team
                     .map(ResponseEntity::ok)
                     .orElse(ResponseEntity.notFound().build());
         } catch (Exception ex) {
-            log.error("Unable to get team for uuid {}.", teamId, ex);
+            log.error("Unable to get team for id {}.", teamId, ex);
             return ResponseEntity.internalServerError().build();
         }
     }
 
     @GetMapping("/teams/{teamId}/matches")
-    public ResponseEntity<List<Match>> getMatches(@PathVariable(name = "teamId") String teamId) {
+    public ResponseEntity<List<Match>> getMatches(
+            @PathVariable(name = "teamId") String teamId,
+            @RequestParam(name = "opus", required = false) Integer opus) {
         try {
-            List<Match> matchesForTeam = matchDomainService.findMatchesForTeam(teamId);
+            List<Match> matchesForTeam = 
+                matchDomainService.findMatchesForTeam(teamId, Optional.ofNullable(opus));
             matchesForTeam.forEach(match -> officialLeagueAndCompetitions.adjustCompetitionName(match.getLeagueId(),
                     match.getCompetitionName(), match::setCompetitionName));
             return ResponseEntity.ok(matchesForTeam);
