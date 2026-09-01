1d8a75f9ffc0cf41495958898834acfbc630cb9b
Dennis <dennis.granasen@gmail.com>
2026-09-01 15:17:57 +0200

    fix: reduce frontend development watchers

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -4,14 +4,15 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-    watchOptions: {
-      ignored: /node_modules/,
-    },
+  watchOptions: {
+    ignored: ['**/node_modules/**', '**/public/**'],
+  },
   devServer: {
     port: 8022,
     allowedHosts: ['localhost'],
     static: {
       directory: './public',
+      watch: false,
     },
   },
 });
